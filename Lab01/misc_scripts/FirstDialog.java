aee3970394bb405c6713e0550ce98e4f15305714
Hoang To An <hoangtoan262003@gmail.com>
2024-09-29 00:04:18 +0700

    Upload work of Lab01

---
+++ Lab01/misc_scripts/FirstDialog.java
@@ -0,0 +1,10 @@
+// Hoàng Tố An - 20214980
+// IDE: none
+
+import javax.swing.JOptionPane;
+public class FirstDialog{
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+		System.exit(0);
+	}
+}
---
+++ Lab01/misc_scripts/HelloWorld.java
@@ -0,0 +1,9 @@
+// Hoàng Tố An - 20214980
+// IDE: none
+
+public class HelloWorld{
+	public static void main(String args[]) {
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	}
+}
