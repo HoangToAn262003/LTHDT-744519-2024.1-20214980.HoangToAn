4b3b93186d0c3ca4924665ee45ba26df9addfdb9
Hoang To An <hoangtoan262003@gmail.com>
2024-11-30 08:29:41 +0700

    Add the classes Disc & Track; modify some constructors

--- Lab04/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
+++ Lab04/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -1,50 +1,17 @@
 package hust.soict.hedspi.aims.media;
 
-public class DigitalVideoDisc extends Media{
-	private String director;
-	private int length;
-
-	private static int nbDigitalVideoDiscs = 0;
-	
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
+public class DigitalVideoDisc extends Disc{
 	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-		this.cost = 0;
-		nbDigitalVideoDiscs++;
-		this.id = nbDigitalVideoDiscs;
+		super(title);
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-		nbDigitalVideoDiscs++;
-		this.id = nbDigitalVideoDiscs;
+		super(title, category, cost);
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
-		nbDigitalVideoDiscs++;
-		this.id = nbDigitalVideoDiscs;
+		super(title, category, director, cost);
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
-		nbDigitalVideoDiscs++;
-		this.id = nbDigitalVideoDiscs;
+		super(title, category, director, length, cost);
 	}
 	
 	public boolean isMatch(String title) {
