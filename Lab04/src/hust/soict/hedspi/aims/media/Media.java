0e62a3f78b3218d40b9b64a3299067c6b1049ef4
Hoang To An <hoangtoan262003@gmail.com>
2024-11-30 07:56:30 +0700

    Add the abstract Media class

---
+++ Lab04/src/hust/soict/hedspi/aims/media/Media.java
@@ -0,0 +1,29 @@
+package hust.soict.hedspi.aims.media;
+
+public abstract class Media {
+	private int id;
+	private String title;
+	private String category;
+	private float cost;
+
+	public Media() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+}
